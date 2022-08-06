0213ee3ee5d455c53b22b3e1754fe6073ad8845b
Farshad Bayat <farshad.bayat@gmail.com>
2022-08-06 19:00:28 +0430

    Refactor structure

---
+++ models/repository.model.ts
@@ -0,0 +1,6 @@
+import { Dictionary } from "./dictionary.model";
+
+export interface Repository extends Dictionary<any>
+{
+  dateTime: Date;
+}
--- services/client.service.ts
+++ services/client.service.ts
@@ -1,10 +1,11 @@
 import { Injectable } from "@angular/core";
-import { HttpClient, HttpParams } from "@angular/common/http";
+import { HttpClient } from "@angular/common/http";
 import { BehaviorSubject, Observable, ReplaySubject } from "rxjs";
 import { Dictionary } from "../models/dictionary.model";
 import { Toaster } from "../modules/toast-notification";
 import { IDataBus } from "../models/data-bus.model";
-import { DataRepository } from "../classes/DataRepository";
+import { UserService } from "./user.service";
+import { Repository } from "../models/repository.model";
 @Injectable({
   providedIn: "root",
 })
@@ -12,7 +13,7 @@ export class ClientService
 {
   private loadingCounter$: BehaviorSubject<number>;
   private sharedBus$: ReplaySubject<IDataBus>;
-  private repository: DataRepository = new DataRepository();
+  private repository: Repository = null;
   private access: Dictionary<Dictionary<boolean>> = {};
 
   get loadingAsObservable(): Observable<number>
@@ -27,7 +28,7 @@ export class ClientService
   {
     this.loadingCounter$ = new BehaviorSubject<number>(0);
     this.sharedBus$ = new ReplaySubject<IDataBus>();
-    this.repository.dateTime = new Date();
+    this.repository = { dateTime: new Date() };
   }
 
   public startLoading(): void
@@ -53,19 +54,15 @@ export class ClientService
     this.sharedBus$.next({ receiver: receiver, sender: sender, data: data });
   }
 
-
-
-  public setData(key: string, data: any): DataRepository
+  public setData(key: string, data: any): Repository
   {
     this.repository[key] = data;
-    this.sendOnBus(key, key, data);
     return this.repository;
   }
 
-  public getData(key: string): any
+  public getData(key: string): any | Repository
   {
-    return this.repository[key] ?? null;
+    return key ? this.repository[key] ?? null : this.repository;
   }
-
 
 }
